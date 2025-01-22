179722f4bf598d79773439dad861977b1b8702c7
Marwa Alazab <104984704+Merve-1@users.noreply.github.com>
2025-01-22 16:30:09 +0200

    Delete frontend directory

--- frontend/src/constants/Colors.jsx
+++
@@ -1,8 +0,0 @@
-const Colors = {
-    primary: "#6f87ff",
-    secondary:"#c17aff",
-    bacgroundGradient: "linear-gradient(to buttom, #d7efff,#ffffff",
-    white:"#ffffff",
-    shadow:"rgba(0,0,0,0.2)",
-};
-export default Colors;
